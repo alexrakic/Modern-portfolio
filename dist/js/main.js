83aa463b029f024ba7113dc9259a6cc2e488b81f
Aleksandra Rakic <49693947+alexrakic@users.noreply.github.com>
2019-08-22 21:47:55 +0200

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,8 +3,7 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-items');
-
+const navItems = document.querySelectorAll('.nav-item');
 // Set Initial State Of Menu
 
 let showMenu = false;
